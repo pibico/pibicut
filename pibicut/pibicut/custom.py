289495b613f950391fcc2bc8ea90321a6a4c3f9a
David Arnold <dgx.arnold@gmail.com>
2023-10-13 14:03:42 +0200

    fix: more reliable file loading also from private files

--- pibicut/pibicut/custom.py
+++ pibicut/pibicut/custom.py
@@ -20,15 +20,9 @@ def get_qrcode(input_data, logo):
   )
   qr.add_data(input_data)
   qr.make(fit=True)
-  path = frappe.utils.get_bench_path()
-  site_name = frappe.utils.get_url().replace("http://","").replace("https://","")
-  if ":" in site_name:
-    pos = site_name.find(":")
-    site_name = site_name[:pos]
   
   if logo:
-    embedded = os.path.join(path, "sites", site_name, 'public', logo[1:])
-    img = qr.make_image(image_factory=StyledPilImage, color_mask=RadialGradiantColorMask(back_color = (255,255,255), center_color = (70,130,180), edge_color = (0,0,0)), module_drawer=GappedSquareModuleDrawer(), eye_drawer=SquareModuleDrawer(), embeded_image_path=embedded)
+    img = qr.make_image(image_factory=StyledPilImage, color_mask=RadialGradiantColorMask(back_color = (255,255,255), center_color = (70,130,180), edge_color = (0,0,0)), module_drawer=GappedSquareModuleDrawer(), eye_drawer=SquareModuleDrawer(), embeded_image_path=logo)
   else:
     img = qr.make_image(image_factory=StyledPilImage, color_mask=RadialGradiantColorMask(back_color = (255,255,255), center_color = (70,130,180), edge_color = (0, 0, 0)), module_drawer=GappedSquareModuleDrawer(), eye_drawer=SquareModuleDrawer())
   #qr = qrcode.make(input_str)
