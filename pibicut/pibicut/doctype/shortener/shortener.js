b1b77ce24c87d9e5e962005ac3daeaa78cb04e35
pibiKoin <segitrack@gmail.com>
2024-03-13 21:47:23 +0100

    modified:   pibicut/pibicut/doctype/shortener/shortener.js 	modified:   pibicut/pibicut/doctype/shortener/shortener.json

--- pibicut/pibicut/doctype/shortener/shortener.js
+++ pibicut/pibicut/doctype/shortener/shortener.js
@@ -4,20 +4,23 @@
 frappe.ui.form.on('Shortener', {
   refresh(frm) {
     var template = '';
+    
     if (frm.doc.__islocal) {
       template = '<img src="" />';
-	  frm.set_df_property('qr_preview', 'options', frappe.render_template(template));
+	    frm.set_df_property('qr_preview', 'options', frappe.render_template(template));
       frm.refresh_field('qr_preview');
     } else {
+      /*
       if (frm.doc.logo) {
-	    frm.set_df_property('logo', 'read_only', 1);    
-	    frm.refresh_field('logo');
+	      frm.set_df_property('logo', 'read_only', 1);    
+	      frm.refresh_field('logo');
       } else {
         frm.set_df_property('logo', 'hidden', 1);    
-	    frm.refresh_field('logo');  
+	      frm.refresh_field('logo');  
       }
+      */
       template = '<img src="' + frm.doc.qr_code + '" width="240px"/>';
-	  frm.set_df_property('qr_preview', 'options', frappe.render_template(template));
+	    frm.set_df_property('qr_preview', 'options', frappe.render_template(template));
       frm.refresh_field('qr_preview');
     }
   },
@@ -25,18 +28,20 @@ frappe.ui.form.on('Shortener', {
     var template = '';
     if (frm.doc.__islocal) {
       template = '<img src="" />';
-	  frm.set_df_property('qr_preview', 'options', frappe.render_template(template));
+	    frm.set_df_property('qr_preview', 'options', frappe.render_template(template));
       frm.refresh_field('qr_preview');
     } else {
+      /*
       if (frm.doc.logo) {
-	    frm.set_df_property('logo', 'read_only', 1);    
-	    frm.refresh_field('logo');
+	      frm.set_df_property('logo', 'read_only', 1);    
+	      frm.refresh_field('logo');
       } else {
         frm.set_df_property('logo', 'hidden', 1);    
-	    frm.refresh_field('logo');  
+	      frm.refresh_field('logo');  
       }
+      */
       template = '<img src="' + frm.doc.qr_code + '" width="240px"/>';
-	  frm.set_df_property('qr_preview', 'options', frappe.render_template(template));
+	    frm.set_df_property('qr_preview', 'options', frappe.render_template(template));
       frm.refresh_field('qr_preview');
     }
   }
